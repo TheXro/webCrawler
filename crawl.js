c7a7e113e163baadd85046f92e86a3ff363ff2e4
TheXro <ravyyadav97@gmail.com>
2023-09-28 02:07:09 +0530

    getting url from html

--- crawl.js
+++ crawl.js
@@ -1,3 +1,6 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
 const normalizeURL = (url) => {
     const newURL = new URL(url);
     let fullPath = `${newURL.host}${newURL.pathname}`
@@ -7,9 +10,20 @@ const normalizeURL = (url) => {
     return fullPath
 }
 
+const getURLsFromHTML = (html, baseURL) => {
+    const dom = new JSDOM(html);
+    const links = dom.window.document.querySelectorAll('a');
+    const urls = []
+    links.forEach(link => {
+        const absLink = new URL(link.href, baseURL);
+        urls.push(absLink.href);
+    })
+    return urls
+}
 
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
 
--- crawl.test.js
+++ crawl.test.js
@@ -1,29 +1,42 @@
-const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { test, expect } = require("@jest/globals");
+const { normalizeURL } = require("./crawl.js");
+const { getURLsFromHTML } = require("./crawl.js");
 
-test('normalizeURL', () => {
-    expect(normalizeURL('https://www.google.com/search')).toBe('www.google.com/search')
+test("normalizeURL", () => {
+  expect(normalizeURL("https://www.google.com/search")).toBe(
+    "www.google.com/search"
+  );
 });
 
+test("normalizeURL slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
 
-test('normalizeURL slash', () => {
-    const input = 'https://blog.boot.dev/path/'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
-  })
-  
-  test('normalizeURL capitals', () => {
-    const input = 'https://BLOG.boot.dev/path'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
-  })
-  
-  test('normalizeURL http', () => {
-    const input = 'http://BLOG.boot.dev/path'
-    const actual = normalizeURL(input)
-    const expected = 'blog.boot.dev/path'
-    expect(actual).toEqual(expected)
-  })
-  
+test("normalizeURL capitals", () => {
+  const input = "https://BLOG.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL http", () => {
+  const input = "http://BLOG.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getting all URLs from HTMl  ", () => {
+  const input =
+    '<a href="https://www.google.com/search">Google</a> <a href="/search/bitch">Bing</a> <a href="/search/ded">Yahoo</a>';
+  const actual = getURLsFromHTML(input, 'https://www.google.com');
+  const expected = [
+    "https://www.google.com/search",
+    "https://www.google.com/search/bitch",
+    "https://www.google.com/search/ded",
+  ];
+  expect(actual).toEqual(expected);
+});
